cd653f6970ea6b555092c67efd3c7e53e7d96954
Dylan Kearney <dylan.kearney@wpengine.com>
2020-08-25 11:38:42 +0100

    Made create account form with submit button and with all the required fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if:  :devise_controller?
+
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :remember_me])
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :email, :password, :remember_me])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :current_password])
+        
+    end
+
 end
