9fc0c48f19ad331ab646cf253a30f9db511c3e33
Dylan Kearney <dylan.kearney@wpengine.com>
2020-08-31 11:52:07 +0100

    Added view to input post details and create (TPOZ-1942)

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
+
+end
