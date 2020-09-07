c44b968b6d3624947d23cae6fce3d8c9f0a853e3
Dylan Kearney <dylan.kearney@wpengine.com>
2020-09-07 18:26:07 +0100

    List of posts now ordered by date with newest first. (TPOZ-1954)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
     end
 
     def index
-        @posts = Post.all
+        @posts = Post.all.order("created_at DESC")   #orders by date created at. newest first
     end
 
     private
