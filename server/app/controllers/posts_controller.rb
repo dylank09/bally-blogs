31a328cc696c50a02ac86e82fa95ebaf586f8c89
Dylan Kearney <dylan.kearney@wpengine.com>
2020-10-16 11:21:47 +0100

    tests

--- server/app/controllers/posts_controller.rb
+++ server/app/controllers/posts_controller.rb
@@ -59,45 +59,6 @@ class PostsController < ApplicationController
         end
     end
 
-    # def edit 
-    #     @post = Post.find(params[:id])
-    #     unless current_user.id == @post.user_id
-    #         render json: {
-    #             status: 500,
-    #             errors: "user not authorised to edit post"
-    #         }
-    #     end
-    # end
-
-    # def update
-    #     @post = Post.find(params[:id])
-    #     if @post.update_attributes(params.require(:post).permit(:content, :title))
-    #         if @post.save
-    #             render json: {
-    #                 status: :success,
-    #                 message: "post was updated",
-    #                 data: @post
-    #             }
-    #         else 
-    #             render json: {
-    #                 status: 500,
-    #                 errors: ['post not updated']
-    #             }
-    #         end
-    #     end
-    # end
-
-    # def destroy
-    #     @post = Post.find(params[:id])   #find the post
-
-    #     unless current_user == @post.user
-    #     end
-
-    #     if @post.soft_delete == nil     #if the post is not deleted, it is equal to nil.
-    #         @post.soft_delete_f         #then delete it
-    #     end
-    # end
-
     private
       
     def post_params
