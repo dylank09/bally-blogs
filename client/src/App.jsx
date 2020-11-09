902e63a003d3903a6684b66c50d056f1bf6173f4
Dylan Kearney <dylan.kearney@wpengine.com>
2020-11-09 14:27:44 +0000

    fix delete button on posts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,87 +1,139 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import Home from './components/Home';
-import Login from './components/registrations/Login';
-import Signup from './components/registrations/Signup';
-import PostPage from './posts/PostPage';
-import NewPost from './posts/NewPost';
-import BlogsApi from './services/BlogsApi';
-import './stylesheets/App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/registrations/Login";
+import Signup from "./components/registrations/Signup";
+import PostPage from "./posts/PostPage";
+import NewPost from "./posts/NewPost";
+import BlogsApi from "./services/BlogsApi";
+import "./stylesheets/App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       isLoggedIn: false,
-      user: {}
-     };
+      user: {},
+    };
   }
   componentDidMount() {
-      this.loginStatus()
-    }
+    this.loginStatus();
+  }
   loginStatus = () => {
-      axios.get('http://localhost:3001/logged_in', {withCredentials: true})
-      .then(response => {
+    axios
+      .get("http://localhost:3001/logged_in", { withCredentials: true })
+      .then((response) => {
         if (response.data.logged_in) {
-          this.handleLogin(response)
+          this.handleLogin(response);
         } else {
-          this.handleLogout()
+          this.handleLogout();
         }
       })
-      .catch(error => console.log('api errors:', error))
-    }
+      .catch((error) => console.log("api errors:", error));
+  };
   handleLogin = (data) => {
-      this.setState({
-        isLoggedIn: true,
-        user: data.user
-      })
-    }
+    this.setState({
+      isLoggedIn: true,
+      user: data.user,
+    });
+  };
   handleLogout = () => {
-      this.setState({
+    this.setState({
       isLoggedIn: false,
-      user: {}
-      })
-    }
+      user: {},
+    });
+  };
   render() {
-      return (
-        <div>
-          <BrowserRouter>
-            <Switch>
-              <Route 
-                exact path='/' 
-                render={props => (
-                <Home {...props} handleLogout={this.handleLogout} loggedInStatus={this.state.isLoggedIn}/>
-                )}
-              />
-              <Route 
-                exact path='/login' 
-                render={props => (
-                <Login {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
-                )}
-              />
-              <Route 
-                exact path='/signup' 
-                render={props => (
-                <Signup {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
-                )}
-              />
-              <Route 
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <Home
+                  {...props}
+                  handleLogout={this.handleLogout}
+                  loggedInStatus={this.state.isLoggedIn}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/login"
+              render={(props) => (
+                <Login
+                  {...props}
+                  handleLogin={this.handleLogin}
+                  loggedInStatus={this.state.isLoggedIn}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/signup"
+              render={(props) => (
+                <Signup
+                  {...props}
+                  handleLogin={this.handleLogin}
+                  loggedInStatus={this.state.isLoggedIn}
+                />
+              )}
+            />
+
+            {this.state.user ? (
+              <Route
                 path="/posts/:id"
-                render={props => (
-                  <PostPage {...props} blogApi={BlogsApi} loggedInStatus={this.state.isLoggedIn} user_id={this.state.user.id}/>
+                render={(props) => (
+                  <PostPage
+                    {...props}
+                    blogApi={BlogsApi}
+                    loggedInStatus={this.state.isLoggedIn}
+                    userId={this.state.user.id}
+                  />
                 )}
               />
-              <Route 
-                exact path="/new" 
-                render={props => (
-                  <NewPost {...props} blogApi={BlogsApi} loggedInStatus={this.state.isLoggedIn}/>
+            ) : (
+              <Route
+                path="/posts/:id"
+                render={(props) => (
+                  <PostPage
+                    {...props}
+                    blogApi={BlogsApi}
+                    loggedInStatus={this.state.isLoggedIn}
+                  />
                 )}
               />
-            </Switch>
-          </BrowserRouter>
-        </div>
-      );
-    }
+            )}
+
+            <Route
+              path="/posts/:id"
+              render={(props) => (
+                <PostPage
+                  {...props}
+                  blogApi={BlogsApi}
+                  loggedInStatus={this.state.isLoggedIn}
+                  userId={this.state.user.id}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/new"
+              render={(props) => (
+                <NewPost
+                  {...props}
+                  blogApi={BlogsApi}
+                  loggedInStatus={this.state.isLoggedIn}
+                />
+              )}
+            />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 export default App;
